9156aea6daacfa50684471837a1f1ce56aa3ca20
KeyRoll <@mail.com>
2019-11-14 11:45:21 +0100

    machine learning notes for the exercices

---
+++ notes/1/pre_process.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 17:13:21 2019
+
+@author: joan
+"""
+# plantilla de pre process
+# como importar librerias: 
+#   numpy
+#   pandas
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd # carga y manipulación de datos 
+
+df = [1,2,3]
+print(df)
---
+++ notes/1/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 15:24:16 2019
+
+@author: joan
+"""
+
+# testing the IDE spyder
+print(1,2,3)
+
